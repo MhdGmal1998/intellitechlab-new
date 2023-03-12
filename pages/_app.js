765667888f21293182abf0e52d18499444ec8676
Mohammed Al-Homaidi <alhmydymhmdjmal91@gmail.com>
2023-03-12 03:46:14 +0300

    dasdf

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/argon-design-system-react.css";
 import "../styles/styles.css";
-import "../styles/vendor/font-awesome/css/font-awesome.min.css";
-import "../styles/vendor/nucleo/css/nucleo.css";
-import '../public/vendor/bootstrap-icons/bootstrap-icons.css'
-import '../public/vendor/boxicons/css/boxicons.min.css'
-import '../public/vendor/glightbox/css/glightbox.min.css'
-import '../public/vendor/remixicon/remixicon.css'
-import '../public/vendor/swiper/swiper-bundle.min.css'
+// import "../styles/vendor/font-awesome/css/font-awesome.min.css";
+// import "../styles/vendor/nucleo/css/nucleo.css";
+// import '../public/vendor/bootstrap-icons/bootstrap-icons.css'
+// import '../public/vendor/boxicons/css/boxicons.min.css'
+// import '../public/vendor/glightbox/css/glightbox.min.css'
+// import '../public/vendor/remixicon/remixicon.css'
+// import '../public/vendor/swiper/swiper-bundle.min.css'
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
